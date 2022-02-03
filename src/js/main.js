40abb2dd1fca557c81f4c8f5e60d02f06b6df0b3
IceDiver <admin@example.com>
2022-02-03 20:47:21 +0300

    add youtube videoplayer feature

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
+const { default: VideoPlayer } = require("./modules/playVideo");
 const { default: Slider } = require("./modules/slider")
 
 window.addEventListener('DOMContentLoaded', () => {
   const slider = new Slider('.page', '.next');
   slider.render();
+
+  const player = new VideoPlayer('.showup .play', '.overlay');
+  player.init();
 })
